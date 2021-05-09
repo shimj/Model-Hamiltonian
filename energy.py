fbe7235b66a73a69221bed61a37cb3fef16e635f
shimj <mjshi94@gmail.com>
2021-05-09 21:38:01 +0800

    delete some files

--- energy.py
+++
@@ -1,8 +0,0 @@
-from model_hamiltonian._tool import get_hermitian_base
-from model_hamiltonian.model_hamiltonian import energy
-a = get_hermitian_base(4)
-for n, item in enumerate(a):
-    print(n, item)
-b = [a[12], a[4], a[8]]
-
-print(energy(b))
